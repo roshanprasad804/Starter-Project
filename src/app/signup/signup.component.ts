65c2b2b5c92517c79a2292bf27c25e9a8fd2acbd
Roshan Prasad <roshanp60@gmail.com>
2024-01-15 16:00:52 +0530

    signup component complete

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -9,12 +10,21 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  constructor(private router: Router) {}
+
   formData = {
     username: '',
     email: '',
     password: '',
     agree: ''
   };
+
+  redirectToLogin() {
+    // Use the Angular router to navigate to the login component
+    this.router.navigate(['/login']); // Replace 'login' with the actual path of your login component
+    console.log("log check");
+  }
 
   onSubmit() {
     console.log('Form submitted with data:', this.formData);
